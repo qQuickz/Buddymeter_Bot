edb1f20a3e68109fd9c779faf4ce82d85414794a
qQuickz <46611059+qQuickz@users.noreply.github.com>
2019-01-29 20:55:24 +0100

    Add files via upload

---
+++ BuddyBot_Quickz/BuddyBot/Form1.Designer.cs
@@ -0,0 +1,323 @@
+﻿namespace BuddyBot
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.speed = new System.Windows.Forms.NumericUpDown();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.label3 = new System.Windows.Forms.Label();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.tabControl1 = new System.Windows.Forms.TabControl();
+            this.tabPage1 = new System.Windows.Forms.TabPage();
+            this.button1 = new System.Windows.Forms.Button();
+            this.tabControl2 = new System.Windows.Forms.TabControl();
+            this.tabPage2 = new System.Windows.Forms.TabPage();
+            this.listView1 = new System.Windows.Forms.ListView();
+            this.columnHeader1 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader2 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.button5 = new System.Windows.Forms.Button();
+            this.label4 = new System.Windows.Forms.Label();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.label5 = new System.Windows.Forms.Label();
+            this.button2 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.speed)).BeginInit();
+            this.tabControl1.SuspendLayout();
+            this.tabPage1.SuspendLayout();
+            this.tabControl2.SuspendLayout();
+            this.tabPage2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Font = new System.Drawing.Font("Arial", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox1.Location = new System.Drawing.Point(12, 23);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(298, 20);
+            this.textBox1.TabIndex = 5;
+            this.textBox1.Text = "https://buddymeter.com/quiz.html?q=2fvptA9#nav_bar";
+            // 
+            // speed
+            // 
+            this.speed.Font = new System.Drawing.Font("Segoe UI Semibold", 9.75F, System.Drawing.FontStyle.Bold);
+            this.speed.Location = new System.Drawing.Point(299, 18);
+            this.speed.Maximum = new decimal(new int[] {
+            2000,
+            0,
+            0,
+            0});
+            this.speed.Name = "speed";
+            this.speed.Size = new System.Drawing.Size(60, 25);
+            this.speed.TabIndex = 6;
+            this.speed.Value = new decimal(new int[] {
+            998,
+            0,
+            0,
+            0});
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Segoe UI Semibold", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(245, 21);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(48, 17);
+            this.label1.TabIndex = 7;
+            this.label1.Text = "Speed:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Segoe UI Semibold", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(9, 2);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(112, 17);
+            this.label2.TabIndex = 9;
+            this.label2.Text = "Buddymeter link:";
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.Black;
+            this.panel1.Location = new System.Drawing.Point(-23, 48);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(497, 10);
+            this.panel1.TabIndex = 10;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Segoe UI Semibold", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(15, 18);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(72, 17);
+            this.label3.TabIndex = 13;
+            this.label3.Text = "Username:";
+            // 
+            // textBox2
+            // 
+            this.textBox2.Font = new System.Drawing.Font("Segoe UI Semibold", 9.75F, System.Drawing.FontStyle.Bold);
+            this.textBox2.Location = new System.Drawing.Point(89, 16);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(69, 25);
+            this.textBox2.TabIndex = 11;
+            this.textBox2.Text = "#Bot1";
+            // 
+            // tabControl1
+            // 
+            this.tabControl1.Controls.Add(this.tabPage1);
+            this.tabControl1.Font = new System.Drawing.Font("Segoe UI Semibold", 9.75F, System.Drawing.FontStyle.Bold);
+            this.tabControl1.Location = new System.Drawing.Point(12, 64);
+            this.tabControl1.Name = "tabControl1";
+            this.tabControl1.SelectedIndex = 0;
+            this.tabControl1.Size = new System.Drawing.Size(404, 87);
+            this.tabControl1.TabIndex = 14;
+            // 
+            // tabPage1
+            // 
+            this.tabPage1.BackColor = System.Drawing.SystemColors.Control;
+            this.tabPage1.Controls.Add(this.textBox2);
+            this.tabPage1.Controls.Add(this.label3);
+            this.tabPage1.Controls.Add(this.speed);
+            this.tabPage1.Controls.Add(this.label1);
+            this.tabPage1.Location = new System.Drawing.Point(4, 26);
+            this.tabPage1.Name = "tabPage1";
+            this.tabPage1.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage1.Size = new System.Drawing.Size(396, 57);
+            this.tabPage1.TabIndex = 0;
+            this.tabPage1.Text = "Bot";
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Segoe UI Semibold", 9.75F, System.Drawing.FontStyle.Bold);
+            this.button1.Location = new System.Drawing.Point(316, 20);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(100, 25);
+            this.button1.TabIndex = 8;
+            this.button1.Text = "->";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click_2);
+            // 
+            // tabControl2
+            // 
+            this.tabControl2.Controls.Add(this.tabPage2);
+            this.tabControl2.Font = new System.Drawing.Font("Segoe UI Semibold", 9.75F, System.Drawing.FontStyle.Bold);
+            this.tabControl2.Location = new System.Drawing.Point(12, 153);
+            this.tabControl2.Name = "tabControl2";
+            this.tabControl2.SelectedIndex = 0;
+            this.tabControl2.Size = new System.Drawing.Size(422, 276);
+            this.tabControl2.TabIndex = 16;
+            // 
+            // tabPage2
+            // 
+            this.tabPage2.BackColor = System.Drawing.SystemColors.Control;
+            this.tabPage2.Controls.Add(this.listView1);
+            this.tabPage2.Location = new System.Drawing.Point(4, 26);
+            this.tabPage2.Name = "tabPage2";
+            this.tabPage2.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage2.Size = new System.Drawing.Size(414, 246);
+            this.tabPage2.TabIndex = 0;
+            this.tabPage2.Text = "Answers";
+            // 
+            // listView1
+            // 
+            this.listView1.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.columnHeader1,
+            this.columnHeader2});
+            this.listView1.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.listView1.Font = new System.Drawing.Font("Segoe UI Semibold", 9.75F, System.Drawing.FontStyle.Bold);
+            this.listView1.GridLines = true;
+            this.listView1.HeaderStyle = System.Windows.Forms.ColumnHeaderStyle.Nonclickable;
+            this.listView1.Location = new System.Drawing.Point(6, 3);
+            this.listView1.Name = "listView1";
+            this.listView1.Size = new System.Drawing.Size(404, 241);
+            this.listView1.TabIndex = 0;
+            this.listView1.UseCompatibleStateImageBehavior = false;
+            this.listView1.View = System.Windows.Forms.View.Details;
+            this.listView1.SelectedIndexChanged += new System.EventHandler(this.listView1_SelectedIndexChanged);
+            // 
+            // columnHeader1
+            // 
+            this.columnHeader1.Text = "Question";
+            this.columnHeader1.Width = 262;
+            // 
+            // columnHeader2
+            // 
+            this.columnHeader2.Text = "Answer";
+            this.columnHeader2.Width = 136;
+            // 
+            // button5
+            // 
+            this.button5.Font = new System.Drawing.Font("Segoe UI Semibold", 9.75F, System.Drawing.FontStyle.Bold);
+            this.button5.Location = new System.Drawing.Point(20, 431);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(274, 25);
+            this.button5.TabIndex = 17;
+            this.button5.Text = "Get Answers / Start Bot";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Segoe UI Black", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(381, 154);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(46, 21);
+            this.label4.TabIndex = 14;
+            this.label4.Text = "0/10";
+            // 
+            // panel2
+            // 
+            this.panel2.BackColor = System.Drawing.Color.Black;
+            this.panel2.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F);
+            this.panel2.Location = new System.Drawing.Point(-15, 461);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(497, 10);
+            this.panel2.TabIndex = 18;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Segoe UI Black", 12F, System.Drawing.FontStyle.Bold);
+            this.label5.Location = new System.Drawing.Point(9, 472);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(64, 21);
+            this.label5.TabIndex = 19;
+            this.label5.Text = "Host: -";
+            // 
+            // button2
+            // 
+            this.button2.Font = new System.Drawing.Font("Segoe UI Semibold", 9.75F, System.Drawing.FontStyle.Bold);
+            this.button2.Location = new System.Drawing.Point(300, 431);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(126, 25);
+            this.button2.TabIndex = 20;
+            this.button2.Text = "Clear User Data";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click_1);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(436, 496);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.panel2);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.tabControl2);
+            this.Controls.Add(this.tabControl1);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label5);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Buddymeter bot by Quickz";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form1_FormClosing);
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.speed)).EndInit();
+            this.tabControl1.ResumeLayout(false);
+            this.tabPage1.ResumeLayout(false);
+            this.tabPage1.PerformLayout();
+            this.tabControl2.ResumeLayout(false);
+            this.tabPage2.ResumeLayout(false);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.NumericUpDown speed;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TabControl tabControl1;
+        private System.Windows.Forms.TabPage tabPage1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TabControl tabControl2;
+        private System.Windows.Forms.TabPage tabPage2;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.ListView listView1;
+        private System.Windows.Forms.ColumnHeader columnHeader1;
+        private System.Windows.Forms.ColumnHeader columnHeader2;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Button button2;
+    }
+}
+
---
+++ BuddyBot_Quickz/BuddyBot/Form1.cs
@@ -0,0 +1,183 @@
+﻿using MSHTML;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace BuddyBot
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        IWebDriver driver = new ChromeDriver();
+        ChromeDriverService service = ChromeDriverService.CreateDefaultService();
+        //javascript:questionArray[question_number].answer
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            driver.Manage().Window.Maximize();
+            service.HideCommandPromptWindow = true;
+
+        }
+        private void button2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click_1(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click_2(object sender, EventArgs e)
+        {
+            if (textBox1.Text.Contains("buddymeter"))
+            {
+                driver.Navigate().GoToUrl(textBox1.Text);
+                label5.Text = "Host: " + driver.FindElement(By.ClassName("quiz_name")).Text;
+                label5.ForeColor = Color.Green;
+            }
+            else
+            {
+                MessageBox.Show("Please Enter a Buddymeter link!", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private void button3_Click_1(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            driver.Close();
+        }
+
+        private void GetAnswer()
+        {
+            IJavaScriptExecutor executor = driver as IJavaScriptExecutor;
+            executor.ExecuteScript("javascript:alert(questionArray[question_number].answer)"); //Javascript inject
+            try
+            {
+                string text = driver.FindElement(By.XPath("//input[@value = 'alert']")).ToString();
+            }
+            catch (Exception zahl)//Finds the answer for question
+            {
+                SendKeys.Send("{ENTER}");
+                string input = zahl.ToString();
+                string digits = new String(input.Take(85).ToArray());
+                string resultString = Regex.Match(digits, @"\d+").Value;
+                Clipboard.SetText(resultString);
+                System.Threading.Thread.Sleep(Convert.ToInt16(25));
+            }
+        }
+        private void button5_Click(object sender, EventArgs e)
+        { 
+            //Automated Javscipt inject and Answer-Sort
+            driver.Manage().Window.Maximize();
+            TopMost = true;
+            driver.FindElement(By.Name("text_option_2")).SendKeys(textBox2.Text);
+            driver.FindElement(By.Id("name_button")).Click();
+            System.Threading.Thread.Sleep(Convert.ToInt16(speed.Value));
+            GetAnswer();
+            driver.FindElement(By.Id("option_" + Clipboard.GetText())).Click();
+            listView1.Items.Add(driver.FindElement(By.Id("question")).Text).SubItems.Add(" (" + Clipboard.GetText() + ") "+ driver.FindElement(By.Id("option_" + Clipboard.GetText())).Text);
+            label4.ForeColor = Color.Green;
+            label4.Text = "1/10";
+            System.Threading.Thread.Sleep(Convert.ToInt16(speed.Value));
+            GetAnswer();
+            driver.FindElement(By.Id("option_" + Clipboard.GetText())).Click();
+            listView1.Items.Add(driver.FindElement(By.Id("question")).Text).SubItems.Add(" (" + Clipboard.GetText() + ") " + driver.FindElement(By.Id("option_" + Clipboard.GetText())).Text);
+            label4.Text = "2/10";
+            System.Threading.Thread.Sleep(Convert.ToInt16(speed.Value));
+            GetAnswer();
+            driver.FindElement(By.Id("option_" + Clipboard.GetText())).Click();
+            listView1.Items.Add(driver.FindElement(By.Id("question")).Text).SubItems.Add(" (" + Clipboard.GetText() + ") " + driver.FindElement(By.Id("option_" + Clipboard.GetText())).Text);
+            label4.Text = "3/10";
+            System.Threading.Thread.Sleep(Convert.ToInt16(speed.Value));
+            GetAnswer();
+            driver.FindElement(By.Id("option_" + Clipboard.GetText())).Click();
+            listView1.Items.Add(driver.FindElement(By.Id("question")).Text).SubItems.Add(" (" + Clipboard.GetText() + ") " + driver.FindElement(By.Id("option_" + Clipboard.GetText())).Text);
+            label4.Text = "4/10";
+            System.Threading.Thread.Sleep(Convert.ToInt16(speed.Value)); 
+            GetAnswer();
+            driver.FindElement(By.Id("option_" + Clipboard.GetText())).Click();
+            listView1.Items.Add(driver.FindElement(By.Id("question")).Text).SubItems.Add(" (" + Clipboard.GetText() + ") " + driver.FindElement(By.Id("option_" + Clipboard.GetText())).Text);
+            label4.Text = "5/10";
+            System.Threading.Thread.Sleep(Convert.ToInt16(speed.Value));
+            GetAnswer();
+            driver.FindElement(By.Id("option_" + Clipboard.GetText())).Click();
+            listView1.Items.Add(driver.FindElement(By.Id("question")).Text).SubItems.Add(" (" + Clipboard.GetText() + ") " + driver.FindElement(By.Id("option_" + Clipboard.GetText())).Text);
+            label4.Text = "6/10";
+            System.Threading.Thread.Sleep(Convert.ToInt16(speed.Value));
+            GetAnswer();
+            driver.FindElement(By.Id("option_" + Clipboard.GetText())).Click();
+            listView1.Items.Add(driver.FindElement(By.Id("question")).Text).SubItems.Add(" (" + Clipboard.GetText() + ") " + driver.FindElement(By.Id("option_" + Clipboard.GetText())).Text);
+            label4.Text = "7/10";
+            System.Threading.Thread.Sleep(Convert.ToInt16(speed.Value));
+            GetAnswer();
+            driver.FindElement(By.Id("option_" + Clipboard.GetText())).Click();
+            listView1.Items.Add(driver.FindElement(By.Id("question")).Text).SubItems.Add(" (" + Clipboard.GetText() + ") " + driver.FindElement(By.Id("option_" + Clipboard.GetText())).Text);
+            label4.Text = "8/10";
+            System.Threading.Thread.Sleep(Convert.ToInt16(speed.Value));
+            GetAnswer();
+            driver.FindElement(By.Id("option_" + Clipboard.GetText())).Click();
+            listView1.Items.Add(driver.FindElement(By.Id("question")).Text).SubItems.Add(" (" + Clipboard.GetText() + ") " + driver.FindElement(By.Id("option_" + Clipboard.GetText())).Text);
+            label4.Text = "9/10";
+            System.Threading.Thread.Sleep(Convert.ToInt16(speed.Value));
+            GetAnswer();
+            driver.FindElement(By.Id("option_" + Clipboard.GetText())).Click();
+            listView1.Items.Add(driver.FindElement(By.Id("question")).Text).SubItems.Add(" (" + Clipboard.GetText() + ") " + driver.FindElement(By.Id("option_" + Clipboard.GetText())).Text);
+            label4.Text = "10/10";
+        }
+
+        private void button3_Click_2(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button4_Click_1(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button2_Click_1(object sender, EventArgs e)
+        {
+            driver.Navigate().GoToUrl("chrome://settings/clearBrowserData"); //Clear Cookie 
+            listView1.Items.Clear();
+            label4.Text = "0/10";
+            label4.ForeColor = Color.Black;
+        }
+
+        private void listView1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
